0368f4f2f37094faa1a3eccbc1b6d29aa49bc212
RyuseiKojima <jiko.likegc@gmail.com>
2026-06-18 23:28:27 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <main className="app">
+            <h1>Todoリスト</h1>
+            <p>Reactでタスク管理アプリを作っていきましょう。</p>
+        </main>
+    );
+}
+
+export default App;
